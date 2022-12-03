6f7a43bf6f6e6f367843819283f8a6d2be0aa0fe
shoppingzh <xpzheng6@163.com>
2022-12-03 17:09:50 +0800

    home animation

--- docs/.vitepress/components/Home/Right.vue
+++ docs/.vitepress/components/Home/Right.vue
@@ -11,22 +11,84 @@
 <script setup></script>
 
 <style scoped>
+@keyframes ani-b1 {
+  0% {
+    @apply right-0 bottom-[400px] opacity-0;
+  }
+  80% {
+    @apply right-0 bottom-0;
+  }
+  90% {
+    @apply right-0 bottom-[10px];
+  }
+  95% {
+    @apply right-0 bottom-0;
+  }
+  97% {
+    @apply right-0 bottom-[3px];
+  }
+  100% {
+    @apply right-0 bottom-0;
+  }
+}
+@keyframes ani-b2 {
+  0% {
+    @apply right-[135px] bottom-[400px];
+  }
+  100% {
+    @apply right-[135px] bottom-[20px] opacity-100;
+  }
+}
+@keyframes ani-b3 {
+  0% {
+    @apply right-[0] bottom-[400px];
+  }
+  100% {
+    @apply right-0 bottom-[194px] opacity-100;
+  }
+}
+@keyframes ani-b4 {
+  0% {
+    @apply left-[-30px] bottom-[400px];
+  }
+  100% {
+    @apply left-[-30px] bottom-[224px] opacity-100;
+  }
+}
+@keyframes ani-b5 {
+  0% {
+    @apply left-[80px] top-[-50px];
+  }
+  100% {
+    @apply left-[80px] top-[-30px] opacity-100;
+  }
+}
+
 img {
   @apply inline absolute;
 }
 .b1 {
-  @apply w-[150px] right-0 bottom-0;
+  @apply w-[150px];
+  animation: ani-b1 1s ease-in-out forwards;
 }
 .b2 {
-  @apply w-[280px] right-[135px] bottom-[20px];
+  @apply w-[280px] opacity-0;
+  animation: ani-b2 0.75s ease-in forwards;
+  animation-delay: 1s;
 }
 .b3 {
-  @apply w-[260px] right-0 bottom-[194px];
+  @apply w-[260px] opacity-0;
+  animation: ani-b3 0.5s ease-in-out forwards;
+  animation-delay: 1.75s;
 }
 .b4 {
-  @apply w-[200px] left-[-30px] bottom-[224px];
+  @apply w-[200px] opacity-0;
+  animation: ani-b4 0.5s ease-in-out forwards;
+  animation-delay: 2.5s;
 }
 .b5 {
-  @apply w-[160px] left-[80px] top-[-30px];
+  @apply w-[160px] opacity-0;
+  animation: ani-b5 0.3s ease-in forwards;
+  animation-delay: 2.75s;
 }
 </style>
--- docs/.vitepress/components/Home/index.vue
+++ docs/.vitepress/components/Home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-32 md:mt-16 md:mx-[10%] flex justify-center items-center">
     <div class="px-8 md:px-20 text-center">
-      <div class="text-4xl md:text-6xl font-semibold tracking-wider md:tracking-[10px] text-[#3b2c90] dark:text-green-300">
+      <div ref="el" class="text-4xl md:text-6xl font-semibold tracking-wider md:tracking-[10px] text-[#3b2c90] dark:text-green-300">
         <div>欢迎来到我的</div>
         <div class="mt-4">工作思维！</div>
       </div>
@@ -12,6 +12,29 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import Right from './Right.vue'
+import TypeIt from 'typeit'
+import { onMounted, ref } from 'vue'
+
+// const el = ref()
+
+// onMounted(() => {
+//   const instance = new TypeIt(el.value, {
+//     breakLines: true,
+//     html: true,
+//     strings: [
+//       'Hello, 我是晓平。',
+//       '很高兴与你相遇，一同学习知识。',
+//       // '',
+//       // '人生到处知何似，应似飞鸿踏雪泥',
+//       // '泥上偶然留指爪，鸿飞哪复计东西',
+//     ],
+//     speed: 50,
+//     // loop: true,
+//     // loopDelay: 15000,
+//   })
+//   instance.go()
+// })
+
 </script>
