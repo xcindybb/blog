18559e207f16e6f1307ff950f5793d6faceaa01e
shoppingzh <xpzheng6@163.com>
2022-12-03 16:43:58 +0800

    update

--- docs/.vitepress/components/Home/index.vue
+++ docs/.vitepress/components/Home/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="mt-32 md:mt-16 md:mx-[10%] flex justify-center items-center">
     <div class="px-8 md:px-20 text-center">
-      <div class="text-6xl font-semibold tracking-[10px] text-[#3b2c90] dark:text-green-300">
+      <div class="text-4xl md:text-6xl font-semibold tracking-wider md:tracking-[10px] text-[#3b2c90] dark:text-green-300">
         <div>欢迎来到我的</div>
         <div class="mt-4">工作思维！</div>
       </div>
-      <div class="mt-6 text-[#3b2c90] dark:text-green-300 text-xl font-semibold">售前思维构思</div>
+      <div class="mt-12 text-[#3b2c90] dark:text-green-300 text-xl font-semibold">售前思维构思</div>
       <img src="./sign.png" class="mt-6 inline w-[100px]">
     </div>
     <Right class="hidden md:block shrink-0" />
